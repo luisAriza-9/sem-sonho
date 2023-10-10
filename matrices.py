3248fe8d210082b96677f14f298c7f31ea7478dd
luis andres ariza osorio <luisandresariza-9@hotmail.com>
2023-10-10 18:52:00 -0500

    ejercicio ventas

---
+++ matrices.py
@@ -0,0 +1,27 @@
+def crearMatrices(fil,col):
+    m = []
+for  i in  range fil:
+
+     fila =[0] * col
+    m. append(fila)
+    
+return m
+
+def printMatriz(mat):
+    
+
+    
+
+
+
+
+
+def printMatriz(mat):
+    pass
+
+matriz = crearMatrices(4,5)
+printMatriz(matriz)
+
+
+ventas_sweetco.pyventas_sweetco.py
+ventas_sweetco.pyventas_sweetco.py
